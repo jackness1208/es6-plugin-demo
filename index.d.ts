7c023868f54d4175997d5a14c1011b8c49932a76
liudaojie <liudaojie@yy.com>
2020-02-22 00:24:17 +0800

    feat: first commit

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+type DemoFn = () => string
+
+export const demoFn: DemoFn
---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function demoFn(t) {
+  const name = 'demoFn';
+  return `my name is ${name}`
+}
+
+Object.defineProperty(exports, "__esModule", {
+    value: !0
+}), exports.demoFn = demoFn;
